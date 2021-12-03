f08bcc64087441fca31003a035f6a1ceea3fee70
Chung Nguyễn <nguyenthachbaochung@gmail.com>
2021-12-04 00:45:30 +0700

    Add project files.

---
+++ ForAsm1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForAsm1.Startup))]
+namespace ForAsm1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
