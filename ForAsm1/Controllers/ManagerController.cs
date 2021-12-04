cb0c173faa4a2a57f5d196d6616d5cd73879fcc9
Chung Nguyễn <nguyenthachbaochung@gmail.com>
2021-12-04 15:56:11 +0700

    CreateNewTeacher

--- ForAsm1/Controllers/ManagerController.cs
+++ ForAsm1/Controllers/ManagerController.cs
@@ -1,7 +1,11 @@
 ﻿using ForAsm1.MainRole;
+using ForAsm1.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,10 +14,70 @@ namespace ForAsm1.Controllers
     [Authorize(Roles = Role.Manager)]
     public class ManagerController : Controller
     {
-        // GET: Admin
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationDbContext _context;
+        public ManagerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ManagerController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            _context = new ApplicationDbContext();
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        // GET: Manager
         public ActionResult CreateTeacher()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateTeacher(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(user.Id, Role.Teacher);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+            return View(model);
         }
         public ActionResult RemoveTeacher()
         {
@@ -23,6 +87,12 @@ namespace ForAsm1.Controllers
         {
             return View();
         }
-
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
     }
 }
