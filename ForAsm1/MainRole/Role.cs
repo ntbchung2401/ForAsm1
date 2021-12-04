cc80355a1796eb0783b5d9ee3f1259c7b40b167e
Chung Nguyễn <nguyenthachbaochung@gmail.com>
2021-12-04 09:00:26 +0700

    AddRoleClass

---
+++ ForAsm1/MainRole/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForAsm1.MainRole
+{
+    public class Role
+    {
+        public const string User = "user";
+        public const string Manager = "manager";
+        public const string Director = "director";
+    }
+}
